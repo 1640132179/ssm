51cf60811d66f26535720b7a757456952b025ab5
zhangsan <zhangsan@itcast.cn>
2019-06-26 16:49:40 +0800

    测试类添加了一个数据

---
+++ Git_test/src/Git_Demo.java
@@ -0,0 +1,7 @@
+public class Git_Demo {
+    public static void main(String[] args) {
+        System.out.println("星纹陨爆");
+        System.out.println("地爆天星");
+        System.out.println("呀~呀~呀");
+    }
+}
